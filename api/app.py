ed2c42fa64f41fb8b75eefdac57306f16e44a5b1
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-01-19 00:08:19 +0530

    app not found

--- api/app.py
+++ api/app.py
@@ -3,12 +3,16 @@ import pickle
 import numpy as np
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "..", "model.pkl")
+
 app = Flask(
     __name__,
-    template_folder="../templates"
+    template_folder="../templates",
+    static_folder="../static"
 )
 
-model = pickle.load(open("/model.pkl", "rb"))
+model = pickle.load(open(MODEL_PATH, "rb"))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -26,6 +30,5 @@ def index():
 
     return render_template("index.html", prediction=prediction)
 
-# Required for Vercel
 def handler(environ, start_response):
     return app(environ, start_response)

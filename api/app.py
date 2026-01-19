b56102d95db1a23b978e4bfd002cc172425d1e25
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2026-01-19 09:35:22 +0530

    Flask ML app deployment

--- api/app.py
+++ api/app.py
@@ -3,16 +3,17 @@ import pickle
 import numpy as np
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_PATH = os.path.join(BASE_DIR, "..", "model.pkl")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(
     __name__,
-    template_folder="../templates",
-    static_folder="../static"
+    template_folder=os.path.join(BASE_DIR, "templates"),
+    static_folder=os.path.join(BASE_DIR, "static")
 )
 
-model = pickle.load(open(MODEL_PATH, "rb"))
+# load model
+model_path = os.path.join(BASE_DIR, "model.pkl")
+model = pickle.load(open(model_path, "rb"))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -29,6 +30,3 @@ def index():
         prediction = round(model.predict(input_data)[0], 2)
 
     return render_template("index.html", prediction=prediction)
-
-def handler(environ, start_response):
-    return app(environ, start_response)
